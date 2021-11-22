9568f8dffdcb755a44adc9af2d95d45fd686b372
Mariusz Jeruzal <mariusz.jeruzal@sts.pl>
2021-11-22 12:04:37 +0100

    added user registered example with PartitionAwareProducerInterface

---
+++ src/Producer/LoggableCallbacks.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Producer;
+
+use Psr\Log\LoggerInterface;
+use StsGamingGroup\KafkaBundle\RdKafka\Callbacks;
+
+trait LoggableCallbacks
+{
+    abstract protected function getLogger(): LoggerInterface;
+
+    public function callbacks(): array
+    {
+        $logger = $this->getLogger();
+
+        return [
+            Callbacks::MESSAGE_DELIVERY_CALLBACK => static function (
+                \RdKafka\Producer $kafkaProducer,
+                \RdKafka\Message $message
+            ) use ($logger) {
+                if ($message->err) {
+                    $logger->emergency(sprintf('Producer: unable to produce message. Error %s', $message->err));
+
+                    return;
+                }
+
+                $logger->notice(
+                    sprintf(
+                        'Producer: message produced. Payload %s | Partition %s | Waiting 10 seconds.',
+                        $message->payload,
+                        $message->partition
+                    )
+                );
+            }
+        ];
+    }
+}
---
+++ src/Producer/UserRegisteredProducer.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Producer;
+
+use App\DTO\UserRegistered;
+use Psr\Log\LoggerInterface;
+use StsGamingGroup\KafkaBundle\Client\Contract\CallableInterface;
+use StsGamingGroup\KafkaBundle\Client\Contract\PartitionAwareProducerInterface;
+use StsGamingGroup\KafkaBundle\Client\Producer\Message;
+use StsGamingGroup\KafkaBundle\Configuration\ResolvedConfiguration;
+
+class UserRegisteredProducer implements PartitionAwareProducerInterface, CallableInterface
+{
+    use LoggableCallbacks;
+
+    public const MAX_PARTITIONS = 16;
+
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
+    public function produce($data): Message
+    {
+        /** @var $data UserRegistered */
+        return new Message(
+            json_encode($data->toArray(), JSON_THROW_ON_ERROR)
+        );
+    }
+
+    public function supports($data): bool
+    {
+        return $data instanceof UserRegistered;
+    }
+
+    public function getPartition($data, ResolvedConfiguration $configuration): int
+    {
+        /** @var $data UserRegistered */
+        return $data->getClientId() % self::MAX_PARTITIONS;
+    }
+
+    protected function getLogger(): LoggerInterface
+    {
+        return $this->logger;
+    }
+}
